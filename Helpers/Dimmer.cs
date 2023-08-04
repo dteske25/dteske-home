02c5ceb1b89c2b4f2732971cea840522cbde549b
Daric Teske <dteske25@users.noreply.github.com>
2023-08-04 00:14:48 -0500

    add dimmer

---
+++ Helpers/Dimmer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeskeHomeAssistant.Helpers
+{
+    public class Dimmer
+    {
+        private int _max;
+        private int _step;
+        private int _possibleSteps;
+        private int _currentStep;
+
+        public Dimmer(int max, int step, int currentStep = 1)
+        {
+            _max = max;
+            _step = step;
+            _possibleSteps = max / step;
+            _currentStep = currentStep;
+        }
+
+        public int Current => _max - _step * _currentStep;
+
+        public int Next()
+        {
+            _currentStep = (_currentStep + 1) % _possibleSteps;
+            return Current;
+        }
+
+        public void SetStep(int step)
+        {
+            if (step < 0)
+            {
+                throw new ArgumentOutOfRangeException("Step must be 0 or greater.");
+            }
+            if (step > _possibleSteps)
+            {
+                throw new ArgumentOutOfRangeException($"Step must be {_possibleSteps} or less.");
+            }
+            _currentStep = step;
+        }
+    }
+}
--- apps/Automations/OfficeAutomation.cs
+++ apps/Automations/OfficeAutomation.cs
@@ -17,24 +17,37 @@ public class OfficeAutomation
             entities.Light.Shapes7b48
         };
 
+        var dimmer = new Dimmer(100, 20, 2);
+
         new MotionBuilder(entities.BinarySensor.OfficeSensorMotion, scheduler, logger)
             .WithMotionAllowed(entities.InputBoolean.OfficeMotionAllowed)
-            .WithOnAction(_ => LightHelpers.TurnOn(officeLights, 60))
-            .WithOffAction(_ => LightHelpers.TurnOff(officeLights))
+            .WithOnAction(_ => LightHelpers.TurnOn(officeLights, dimmer.Current))
+            .WithOffAction(_ =>
+            {
+                LightHelpers.TurnOff(officeLights);
+                dimmer.SetStep(2);
+            }, TimeSpan.FromHours(1))
             .Build();
 
         ha.Events.Where(ZigbeeDeviceName.OfficeButton, ZigbeeButtonCommands.LongPress).Subscribe(_ =>
         {
             entities.InputBoolean.OfficeMotionAllowed.TurnOff();
             LightHelpers.TurnOff(officeLights);
+            dimmer.SetStep(2);
             scheduler.Schedule(TimeSpan.FromHours(2), () => entities.InputBoolean.OfficeMotionAllowed.TurnOn());
         });
 
         ha.Events.Where(ZigbeeDeviceName.OfficeButton, ZigbeeButtonCommands.Press).Subscribe(_ =>
         {
             entities.InputBoolean.OfficeMotionAllowed.TurnOff();
-            LightHelpers.TurnOn(officeLights, 60);
+            LightHelpers.TurnOn(officeLights, dimmer.Current);
             scheduler.Schedule(TimeSpan.FromHours(1), () => entities.InputBoolean.OfficeMotionAllowed.TurnOn());
+        });
+
+        ha.Events.Where(ZigbeeDeviceName.OfficeButton, ZigbeeButtonCommands.DoublePress).Subscribe(_ =>
+        {
+            LightHelpers.TurnOn(officeLights, dimmer.Next());
+            logger.LogInformation("New brightness is {@brightness}", dimmer.Current);
         });
     }
 }
