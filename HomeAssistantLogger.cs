b9fa7fff1fb13db32387fdd52d33044a4cd5a0e6
Daric Teske <dteske25@users.noreply.github.com>
2024-01-23 22:24:44 -0600

    update packages

---
+++ HomeAssistantLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace TeskeHomeAssistant;
+
+public static class CustomLoggingProvider
+{
+    /// <summary>
+    ///     Adds standard Serilog logging configuration, from appsettings, as per:
+    ///     https://github.com/datalust/dotnet6-serilog-example
+    /// </summary>
+    /// <param name="builder"></param>
+    public static IHostBuilder UseCustomLogging(this IHostBuilder builder)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();       
+
+        return builder.UseSerilog(logger);
+    }
+}
