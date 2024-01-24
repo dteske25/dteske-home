c69e9d3b1ca57a3fc1939b19f0c23e37b411a616
Daric Teske <dteske25@users.noreply.github.com>
2024-01-23 23:18:48 -0600

    fix office routine

--- apps/OfficeAutomation.cs
+++ apps/OfficeAutomation.cs
@@ -5,19 +5,19 @@ public class OfficeAutomation
 {
     public OfficeAutomation(IHaContext ha, IScheduler scheduler, Entities entities, ILogger<OfficeAutomation> logger)
     {
-        var officeLights = new List<Entity>
-        {
-            entities.Light.OfficeLights,
-        };
-
         var dimmer = new Dimmer(100, 20, 2);
 
         new MotionBuilder(entities.BinarySensor.OfficeMotionSensor, scheduler, logger)
             .WithMotionAllowed(entities.InputBoolean.OfficeMotionAllowed)
-            .WithOnAction(_ => officeLights.TurnOn(dimmer.Current))
+            .WithOnAction(_ =>
+            {
+                entities.Light.Shapes7b48.TurnOn();
+                entities.Light.OfficeLights.TurnOn(brightnessPct: dimmer.Current);
+            })
             .WithOffAction(_ =>
             {
-                officeLights.TurnOff();
+                entities.Light.OfficeLights.TurnOff();
+                entities.Light.Shapes7b48.TurnOff();
                 dimmer.SetStep(2);
             }, TimeSpan.FromHours(1))
             .Build();
@@ -25,7 +25,8 @@ public class OfficeAutomation
         ha.Events.Where(ZigbeeDeviceName.OfficeButton, ZigbeeButtonCommands.Press).Subscribe(_ =>
         {
             entities.InputBoolean.OfficeMotionAllowed.TurnOff();
-            officeLights.TurnOn(dimmer.Next());
+            entities.Light.OfficeLights.TurnOn(brightnessPct: dimmer.Next());
+            entities.Light.Shapes7b48.TurnOn();
             logger.LogInformation("New brightness is {@brightness}", dimmer.Current);
             scheduler.Schedule(TimeSpan.FromHours(1), () => entities.InputBoolean.OfficeMotionAllowed.TurnOn());
         });
@@ -39,7 +40,8 @@ public class OfficeAutomation
         {
             entities.InputBoolean.OfficeMotionAllowed.TurnOff();
             entities.Switch.Outlet1.TurnOff();
-            officeLights.TurnOff();
+            entities.Light.OfficeLights.TurnOff();
+            entities.Light.Shapes7b48.TurnOff();
 
             dimmer.SetStep(2);
             scheduler.Schedule(TimeSpan.FromHours(2), () => entities.InputBoolean.OfficeMotionAllowed.TurnOn());
